960908425b62105ef5b90e852c692979384b69ea
Juan Jose Aguilarte <barelrincon@gmail.com>
2022-05-18 09:08:33 +0200

    Anadido variable de session para el nombre

--- routes/index.js
+++ routes/index.js
@@ -3,14 +3,28 @@ const router = express.Router();
 const Mensaje = require('../models/mensaje.model');
 
 /* GET home page. */
-router.get('/', async function (req, res, next) {
+
+router.get('/', (req, res) => {
+  res.render('index');
+});
+
+router.post('/login', (req, res) => {
+  req.session.nombre = req.body.nombre;
+  res.redirect('/chat');
+});
+
+router.get('/chat', async function (req, res) {
+  console.log(req.session.nombre);
 
   const mensajes = await Mensaje.find()
     .sort({ createdAt: -1 })
     .limit(5)
     .exec();
   
-  res.render("index", { mensajes: mensajes.reverse() });
+  res.render("chat", {
+    mensajes: mensajes.reverse(),
+    nombre: req.session.nombre || "Anonimo"
+  });
 });
 
 module.exports = router;
